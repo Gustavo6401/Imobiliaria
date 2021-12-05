c0fdbcf331eac210bcbf8cfa64eeeac5d2a461a4
Gustavo6401 <gsoey2019123@gmail.com>
2021-12-05 20:56:14 -0300

    Sistema baseado apenas no Front-End(Interface, com o objetivo de funcionar como o sistema de uma empresa que aluga imóveis

---
+++ Imobiliaria/script/hora.js
@@ -0,0 +1,7 @@
+    var data = new Date();
+
+    var hours = data.getHours();
+    var minutes = data.getMinutes();
+    var seconds = data.getSeconds();
+
+    alert('Boa tarde! São exatamente: ' + hours + ':' + minutes + ':' + seconds);
